7cc1d1746dc11eba9e583d80ae93df9286786735
Burt Beckwith <burt@burtbeckwith.com>
2011-03-27 12:36:31 -0400

    1.0.2 release

--- SpringSecurityOpenidGrailsPlugin.groovy
+++ SpringSecurityOpenidGrailsPlugin.groovy
@@ -29,9 +29,9 @@ import org.springframework.security.openid.OpenIDAuthenticationFilter
 
 class SpringSecurityOpenidGrailsPlugin {
 
-	String version = '1.0.1'
+	String version = '1.0.2'
 	String grailsVersion = '1.2.3 > *'
-	Map dependsOn = [springSecurityCore: '1.0 > *']
+	Map dependsOn = [springSecurityCore: '1.1.1 > *']
 	List pluginExcludes = [
 		'grails-app/domain/**',
 		'docs/**',
