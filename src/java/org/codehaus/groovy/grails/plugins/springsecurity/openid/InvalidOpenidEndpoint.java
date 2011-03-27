81fd1d87e13e443c71686dc2a488c8841e5a0f36
Burt Beckwith <burt@burtbeckwith.com>
2011-03-27 12:50:13 -0400

    minor cleanup

--- src/java/org/codehaus/groovy/grails/plugins/springsecurity/openid/InvalidOpenidEndpoint.java
+++ src/java/org/codehaus/groovy/grails/plugins/springsecurity/openid/InvalidOpenidEndpoint.java
@@ -1,21 +1,42 @@
+/* Copyright 2010-2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.codehaus.groovy.grails.plugins.springsecurity.openid;
 
-import org.springframework.security.core.*;
+import java.util.Map;
+
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.ExceptionMappingAuthenticationFailureHandler;
 
 /**
- * Will be throws where there was used and invalid OpenID identifier (invalid URL, target 
- * server not answering, or haven't openid endpoint).   
- * 
- * Allows you to configure handler for this type of exception 
+ * Will be thrown where an invalid OpenID identifier was used (invalid URL, target
+ * server not answering, or haven't openid endpoint).
+ *
+ * Allows you to configure handler for this type of exception
  * (see {@link ExceptionMappingAuthenticationFailureHandler#setExceptionMappings(Map failureUrlMap)}
- * 
+ *
  * @author <a href="http://igorartamonov.com">Igor Artamonov</a>
  */
-@SuppressWarnings("serial")
-class InvalidOpenidEndpoint extends AuthenticationException {
-	
-	public InvalidOpenidEndpoint(Throwable t) {
-		super("Invalid OpenId endpoint", t);
+public class InvalidOpenidEndpoint extends AuthenticationException {
+
+	private static final long serialVersionUID = 1;
+
+	/**
+	 * Constructor.
+	 * @param e the real AuthenticationException
+	 */
+	public InvalidOpenidEndpoint(AuthenticationException e) {
+		super("Invalid OpenId endpoint", e);
 	}
 }
--- src/java/org/codehaus/groovy/grails/plugins/springsecurity/openid/OpenIdAuthenticationFailureHandler.java
+++ src/java/org/codehaus/groovy/grails/plugins/springsecurity/openid/OpenIdAuthenticationFailureHandler.java
@@ -51,11 +51,11 @@ public class OpenIdAuthenticationFailureHandler extends AjaxAwareAuthenticationF
 	@Override
 	public void onAuthenticationFailure(final HttpServletRequest request, final HttpServletResponse response,
 			final AuthenticationException exception) throws IOException, ServletException {
-		
+
 		if (exception.getMessage().contains("Unable to process claimed identity")) { //TODO Not the best way
-            super.onAuthenticationFailure(request, response, new InvalidOpenidEndpoint(exception));
+			super.onAuthenticationFailure(request, response, new InvalidOpenidEndpoint(exception));
 			return;
-        }
+		}
 
 		boolean createMissingUsers = Boolean.TRUE.equals(
 				ReflectionUtils.getConfigProperty("openid.registration.autocreate"));
